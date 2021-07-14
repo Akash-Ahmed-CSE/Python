c3397ce70f7c23a7b200f38385723c0be938b002
Akash Ahmed <57281241+Akash-Ahmed-CSE@users.noreply.github.com>
2021-07-14 18:00:40 +0600

    Add files via upload

---
+++ HakerRank(Problem Solving)/Migratory Birds.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+birds = list(map(int, input().split()))
+
+coutns = [0]*n #[000]
+
+for i in birds:
+    coutns[i]+= 1
+
+
+#print(birds)
+#print(coutns)
+
+
+print(coutns.index(max(coutns)))
