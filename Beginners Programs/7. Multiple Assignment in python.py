260f1f26966b8049b94d54988231339d1c376267
Akash Ahmed <57281241+Akash-Ahmed-CSE@users.noreply.github.com>
2021-07-18 12:38:48 +0600

    Add files via upload

---
+++ Beginners Programs/7. Multiple Assignment in python.py
@@ -0,0 +1,5 @@
+a = b = c = 0;
+print(a,b,c)
+
+x,y,z = 1,2,3
+print(x,y,z)
---
+++ Beginners Programs/8. None And numeric data type in python.py
@@ -0,0 +1,27 @@
+"""
+None: A variavle that is not assigned any value is known as none.
+Numeric: int, float, complex, bool
+list
+Tuple
+Set
+String
+Range
+Dictonary
+"""
+a = 4
+print(a)
+print(type(a))
+b = 4.45
+print(b)
+print(type(b))
+
+x = 5
+y = 6
+print(complex(x, y))
+
+c = x < y
+d = x > y
+print(c)
+print(int(c))
+print(d)
+print(int(d))
